1936a3ef62f1be8807233458c01b7d51c7a60673
Andreas Sihotang <116864427+BangKumish@users.noreply.github.com>
2024-08-12 04:28:53 +0700

    Better still hardcoded lol

--- dist/js/script.js
+++ dist/js/script.js
@@ -19,3 +19,41 @@ hamburger.addEventListener('click', function(){
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 })
+
+// Typewriting Animation
+const typewriterText = document.querySelector(".typewriter");
+const phrases = [
+  "අයුබෝවන්   🙏",
+  "Hello 👋",
+  "Bonjour 👋",
+  "你好 🙌",
+  "Привет 🙏",
+  "こんにちは 🙏",
+  "வணக்கம் 🖐"
+];
+
+let phraseIndex = 0;
+let characterIndex = 0;
+
+function type() {
+  if (characterIndex < phrases[phraseIndex].length) {
+    typewriterText.textContent += phrases[phraseIndex][characterIndex];
+    characterIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 1000);
+  }
+}
+
+function erase() {
+  if (characterIndex > 0) {
+    typewriterText.textContent = phrases[phraseIndex].substring(0, characterIndex - 1);
+    characterIndex--;
+    setTimeout(erase, 50);
+  } else {
+    phraseIndex = (phraseIndex + 1) % phrases.length;
+    setTimeout(type, 250);
+  }
+}
+
+type();
