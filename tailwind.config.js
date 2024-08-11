371104a35988138f0502729ca72b7dde91d79354
Andreas Sihotang <116864427+BangKumish@users.noreply.github.com>
2024-08-11 22:52:39 +0700

    minor changes

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#18181b",
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+};
